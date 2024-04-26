574491e8b93bdf5a456e2f57cd9f551473230c8a
Naki-ym <0141tsubuann@gmail.com>
2024-04-26 23:39:11 +0900

    update: _template input.rb

--- _template/input.rb
+++ _template/input.rb
@@ -8,12 +8,12 @@ s = gets.chomp
 a, b, c = gets.split.map(&:to_i)
 
 # 1行分のスペース区切り数列を配列に
-a = gets.split.map(&:to_i)
+arr = gets.split.map(&:to_i)
 
 # n行分の数値を取得（各行1つ）
 n.times do |i|
-  a << gets.chomp.to_i
+  arr << gets.chomp.to_i
 end
 
 #不特定行数の数値を取得し配列に
-a = readlines.map(&:to_i)
+arr = readlines.map(&:to_i)
